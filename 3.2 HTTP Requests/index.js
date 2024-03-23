ef91b73e90ca573fa8d9a3efb973f8c8c9b4037a
Ryan Dempsey <rjd9896@gmail.com>
2024-03-24 00:08:23 +0900

    Update index.js

--- 3.2 HTTP Requests/index.js
+++ 3.2 HTTP Requests/index.js
@@ -3,9 +3,16 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello</h1>");
+    res.send("<h1>Homepage</h1>");
 })
 
+app.get("/contact", (req, res) => {
+    res.send("555-555-5555");
+})
+
+app.get("/about", (req, res) => {
+    res.send("I'm a dude, playing a dude, disguised as another dude.");
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
